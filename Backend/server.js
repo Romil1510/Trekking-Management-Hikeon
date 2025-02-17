1b60226d13bcd962ba60c1a063be3c1cf80ca17f
Romil1510 <romil.rp1510@gmail.com>
2025-02-17 12:19:05 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -397,4 +397,4 @@ app.post("/api/logout", verifyToken, (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ('0.0.0.0'),() => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
